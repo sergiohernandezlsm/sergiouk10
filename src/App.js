5c5e15c5ee12c6b5f3ec847f25d9026793f3e520
Sergio <sergio.hernandez@littlestarmedia.com>
2021-03-12 08:49:20 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TEst  <code>src/App.js</code> and
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          testor
         </a>
       </header>
     </div>
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const Header = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Sergio Htest
+    </Button>
+  );
+}
+
+export default Header;
