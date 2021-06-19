24407147f6cd629a0979e2790dbc40eb23fd4f9c
Sergio <sergio.hernandez@littlestarmedia.com>
2021-06-19 17:53:30 +0100

    some details

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import css from './Header.module.scss';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export default function Header() {
   return (
     <div className={css.header}>
-    <Jumbotron className={css.topImageWrapper}>
+    <div className={css.topImageWrapper}>
       <h1 className={css.headerText}>{`Sergio Hernández`}<span>{`Web Developer`}</span></h1>
-    </Jumbotron>
+    </div>
     </div>
   );
 }
